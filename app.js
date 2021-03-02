33cb503fdddc7562d288f5454d1e71ce40689663
Robert Hung Nguyen <nthung2411@gmail.com>
2021-03-02 23:39:56 +0700

    add logger js for event emitter

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 function sayHello(name) {
     console.log(`Hello ${name}`);
 }
-
-sayHello('Robert');
+const Logger = require('./logger');
+const logger = new Logger();
+logger.on('messageLogged', (data) => {
+    sayHello(data);
+});
+logger.log('Robert');
+logger.log('Robert Hung Nguyen');
